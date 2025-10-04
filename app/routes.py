5917d36bfecd4376f272a5a5d692773f5d7e0411
JasonSoo123 <soojason4@gmail.com>
2025-10-04 15:24:02 -0700

    gemeni api key added

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
-
+import google.generativeai as genai
+API_KEY = "***REMOVED***"
+genai.configure(api_key=API_KEY)
 bp = Blueprint("main", __name__)
 
 @bp.get("/")
 def home():
     # Renders the landing page with logo + heading
+    model = genai.GenerativeModel('gemini-2.5-flash')
     return render_template("home.html", title="Turtle Trips")
